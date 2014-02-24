dfa93bed257cb964f3b90d1faec054e2911ae848
John DeSilva <desilvjo@umich.edu>
2014-02-24 18:30:08 -0500

    Initial commit of android project

---
+++ src/edu/umich/magicalbus/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.umich.magicalbus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
