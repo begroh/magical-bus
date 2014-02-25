9a2d9e1627f05292ee21a40f709bc05deada4094
John DeSilva <desilvjo@umich.edu>
2014-02-25 12:17:21 -0500

    Added empty Stop model

---
+++ src/edu/umich/magicalbus/model/Stop.java
@@ -0,0 +1,23 @@
+package edu.umich.magicalbus.model;
+
+import org.w3c.dom.Node;
+
+/*
+ * <stop>
+ *	<name>Bonisteel and Beal (Cooley) W</name>
+ *	<name2>Cooley Lab</name2>
+ *	<name3>None</name3>
+ *	<latitude>42.29056</latitude>
+ *	<longitude>-83.71385</longitude>
+ *	<toacount>0</toacount>
+ * </stop> 
+ *
+ */
+
+public class Stop {
+
+	public Stop(Node xmlStopNode) {
+		//TODO
+	}
+
+}
